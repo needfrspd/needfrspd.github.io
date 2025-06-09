e10512397f2b71265564128d5cc16dc6d91c2678
Gagandeep Singh <gagandeep.singh.sde@gmail.com>
2025-06-09 00:41:35 -0400

    update base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   outDir: "docs",
-  base: "/",
+  base: "/needfrspd.github.io",
   site: "https://needfrspd.github.io",
   vite: {
     plugins: [tailwindcss()],
